7220f22c4f73607a794df6cd6aa58b7b7a1ee240
Lidiman <bamaground@gmail.com>
2026-04-13 09:06:22 +0700

    Revert "Making a CRUD Page for Admin in the Admin Panel"
    This reverts commit 760e87560877e65bf5a76c6300ac53a697c8c834.

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
-use App\Models\User;
-use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Dummy Admin Login
-Route::get('/login', [AdminController::class, 'showLogin'])->name('login');
-Route::post('/login', [AdminController::class, 'login']);
-Route::get('/logout', [AdminController::class, 'logout']);
-
-// Admin routes - protected by middleware later
-Route::middleware('admin')->group(function () {
-    Route::get('/admin', [AdminController::class, 'dashboard']);
-    Route::resource('/admin/users', AdminController::class);
-});
-
